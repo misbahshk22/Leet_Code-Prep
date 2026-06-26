4e3b59484881aa5b26f1492fa6911aea1e406670
misbahshk22 <2025sse73@student.uet.edu.pk>
2026-06-26 16:53:43 +0500

    Initial commit for new repo

---
+++ Contains Duplicate.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+
+vector<int> merge(vector<int>left , vector<int> right){
+    vector<int> result;
+    int i=0;
+    int j=0;
+    while(i<left.size() && j<right.size()){
+        if(left[i]<=right[j]){
+            result.push_back(left[i++]);
+        }
+        else{
+            result.push_back (right[j++]);
+
+        }
+
+    }
+
+while(i<left.size()){
+    result.push_back(left[i++]);
+}
+while(j<right.size()){
+    result.push_back(right[j++]);
+}
+return result;
+}
+
+vector<int>mergeSort(vector<int> & nums){
+    if(nums.size()<=1){
+        return nums;
+    }
+  int mid = nums.size()/2;
+  vector<int>left(nums.begin() ,nums.begin() + mid);
+  vector<int>right(nums.begin() + mid , nums.end());
+
+  left = mergeSort(left);
+  right = mergeSort(right); 
+
+  return merge(left , right);
+}
+
+    bool containsDuplicate(vector<int>& nums) {
+        nums =mergeSort(nums);
+        for(int  i= 0;i+1<(int) nums.size();i++){
+            if(nums[i]==nums[i+1]){
+            return true;
+            }
+        }
+        return false;
+    }
+
+};
